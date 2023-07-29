c1acb8bffe3215f6a929f8725bb80efefe5094fe
Simon <44496148+siim0n@users.noreply.github.com>
2023-07-30 01:09:54 +0200

    fix ts

--- lib/mongoclient.ts
+++ lib/mongoclient.ts
@@ -1,6 +1,10 @@
 // This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
 import { MongoClient } from "mongodb";
 
+declare global {
+  var _mongoClientPromise: Promise<MongoClient>;
+}
+
 if (!process.env.MONGO_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
 }
