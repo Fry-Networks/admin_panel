284ca01fb55a7e9d2b41ce1e1aba7b69e27d4367
yoyumiracle <157239347+yoyumiracle@users.noreply.github.com>
2024-09-21 07:41:49 +0900

    add staking amount to reward tab

--- app/table-products.tsx
+++ app/table-products.tsx
@@ -10,19 +10,26 @@ import {
   Callout,
   NumberInput,
   TextInput,
-  Flex,
+  Flex
 } from '@tremor/react';
-import { CheckCircleIcon } from "@heroicons/react/24/solid";
+import { CheckCircleIcon } from '@heroicons/react/24/solid';
 import Modal from 'react-modal';
 import { webUser } from '../lib/webusers-model';
 import { Product, ProductModel } from '../lib/products-schema';
 import { useRef, useState } from 'react';
 import ReactModal from 'react-modal';
+import './css/table-product.css';
 
-export default function ProductsTable({ products, updateProducts }: { products: Product[], updateProducts: Function }) {
+export default function ProductsTable({
+  products,
+  updateProducts
+}: {
+  products: Product[];
+  updateProducts: Function;
+}) {
   const [editingProduct, setEditingProduct] = useState<Product | null>(null);
   const [globalMultiplier, setGlobalMultiplier] = useState(1);
-  const [updateSuccess, setUpdateSuccess] = useState(""); // State to track update success
+  const [updateSuccess, setUpdateSuccess] = useState(''); // State to track update success
 
   const openEditModal = (product: Product) => {
     setEditingProduct(product);
@@ -32,6 +39,8 @@ export default function ProductsTable({ products, updateProducts }: { products: 
   };
   const unverifiedRewardRef = useRef<HTMLInputElement>(null);
   const verifiedRewardRef = useRef<HTMLInputElement>(null);
+  const stakeAmountHighRef = useRef<HTMLInputElement>(null);
+  const stakeAmountLowRef = useRef<HTMLInputElement>(null);
   const globalMultiplierRef = useRef<HTMLInputElement>(null);
   const handleSubmit = async (e: any) => {
     e.preventDefault();
@@ -44,9 +53,16 @@ export default function ProductsTable({ products, updateProducts }: { products: 
 
     const unverifiedReward = unverifiedRewardRef.current?.value;
     const verifiedReward = verifiedRewardRef.current?.value;
+    const stakeAmountHigh = stakeAmountHighRef.current?.value;
+    const stakeAmountLow = stakeAmountLowRef.current?.value;
 
     // Ensure the values are retrieved
-    if (unverifiedReward === undefined || verifiedReward === undefined) {
+    if (
+      unverifiedReward === undefined ||
+      verifiedReward === undefined ||
+      stakeAmountHigh === undefined ||
+      stakeAmountLow === undefined
+    ) {
       console.error('Form elements are missing');
       return;
     }
@@ -54,22 +70,25 @@ export default function ProductsTable({ products, updateProducts }: { products: 
     const updateData = {
       productId: editingProduct.wix_id, // Use the appropriate identifier for the product
       unverifiedReward: unverifiedReward,
-      verifiedReward: verifiedReward
+      verifiedReward: verifiedReward,
+      stakeAmountHigh: stakeAmountHigh,
+      stakeAmountLow: stakeAmountLow
     };
 
     try {
       console.log('Updating product:', editingProduct);
 
-      const response = await fetch('/api/edit-product', { // Replace with your actual API endpoint
+      const response = await fetch('/api/edit-product', {
+        // Replace with your actual API endpoint
         method: 'PUT',
         headers: {
-          'Content-Type': 'application/json',
+          'Content-Type': 'application/json'
         },
-        body: JSON.stringify(updateData),
+        body: JSON.stringify(updateData)
       });
 
       if (!response.ok) {
-        setUpdateSuccess("error"); // Reset success state
+        setUpdateSuccess('error'); // Reset success state
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
 
@@ -77,7 +96,7 @@ export default function ProductsTable({ products, updateProducts }: { products: 
       console.log('Updated product:', result);
       updateProducts(); // Update products table
       setUpdateSuccess(editingProduct.name); // Set success state to true
-      setTimeout(() => setUpdateSuccess(""), 3000); // Reset success state after 3 seconds
+      setTimeout(() => setUpdateSuccess(''), 3000); // Reset success state after 3 seconds
     } catch (err) {
       console.error('Error updating product:', err);
     }
@@ -89,27 +108,28 @@ export default function ProductsTable({ products, updateProducts }: { products: 
 
   const updateMultiplier = async () => {
     try {
-      const response = await fetch('/api/update-multiplier', { // Replace with your actual API endpoint
+      const response = await fetch('/api/update-multiplier', {
+        // Replace with your actual API endpoint
         method: 'PUT',
         headers: {
-          'Content-Type': 'application/json',
+          'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ multiplier: globalMultiplier }),
+        body: JSON.stringify({ multiplier: globalMultiplier })
       });
 
       if (!response.ok) {
-        setUpdateSuccess("error"); // Reset success state
+        setUpdateSuccess('error'); // Reset success state
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
 
       const result = await response.json();
       console.log('Updated multiplier:', result);
-      setUpdateSuccess("multiplier"); // Set success state to true
-      setTimeout(() => setUpdateSuccess(""), 3000); // Reset success state after 3 seconds
+      setUpdateSuccess('multiplier'); // Set success state to true
+      setTimeout(() => setUpdateSuccess(''), 3000); // Reset success state after 3 seconds
     } catch (err) {
       console.error('Error updating multiplier:', err);
     }
-  }
+  };
 
   function formatDate(date: Date) {
     date = new Date(date);
@@ -123,63 +143,102 @@ export default function ProductsTable({ products, updateProducts }: { products: 
   }
 
   return (
-    <div>
-      {(updateSuccess != "" && updateSuccess != "error") && (
-        <Callout className="mt-4" title="Success" icon={CheckCircleIcon} color="teal">
+    <div className="product-container">
+      {/* Success and Error messages */}
+      {updateSuccess !== '' && updateSuccess !== 'error' && (
+        <Callout
+          className="mt-4"
+          title="Success"
+          icon={CheckCircleIcon}
+          color="teal"
+        >
           Successfully updated {updateSuccess} !
         </Callout>
       )}
-      {(updateSuccess == "error") && (
-        <Callout className="mt-4" title="Error" icon={CheckCircleIcon} color="red">
+      {updateSuccess === 'error' && (
+        <Callout
+          className="mt-4"
+          title="Error"
+          icon={CheckCircleIcon}
+          color="red"
+        >
           Error updating product!
         </Callout>
       )}
-      <Flex flexDirection='row' className="mt-6">
-        <NumberInput ref={globalMultiplierRef} defaultValue={globalMultiplier} step={0.01} onChange={(e) => setGlobalMultiplier(+e.target.value)} />
-        <Button className="ml-4" onClick={() => {
-          updateMultiplier();
-        }}>Update multiplier</Button>
-        
+
+      {/* Input and Button for updating multiplier */}
+      <Flex flexDirection="row" className="multiplier-row">
+        <NumberInput
+          ref={globalMultiplierRef}
+          defaultValue={globalMultiplier}
+          step={0.01}
+          onChange={(e) => setGlobalMultiplier(+e.target.value)}
+          className="number-input"
+        />
+        <Button
+          className="ml-4 update-button"
+          onClick={() => updateMultiplier()}
+        >
+          Update multiplier
+        </Button>
       </Flex>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableHeaderCell>Name</TableHeaderCell>
-            <TableHeaderCell>Key</TableHeaderCell>
-            <TableHeaderCell>Unverified rewards</TableHeaderCell>
-            <TableHeaderCell>Verified rewards</TableHeaderCell>
-            <TableHeaderCell>Added on </TableHeaderCell>
-            <TableHeaderCell>Actions</TableHeaderCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {products.map((product) => (
-            <TableRow key={product.wix_id}>
-              <TableCell>{product.name}</TableCell>
-              <TableCell>
-                <Text>{product.key}</Text>
-              </TableCell>
-              <TableCell>
-                <Text>{product.reward.unverified}</Text>
-              </TableCell>
-              <TableCell>
-                <Text>{product.reward.verified}</Text>
-              </TableCell>
-              <TableCell>
-                <Text>{product.created_at ? formatDate(product.created_at) : "Unknown"}</Text>
-              </TableCell>
-              <TableCell>
-                <Button
-                  variant="secondary"
-                  onClick={() => openEditModal(product)}
-                >
-                  Edit
-                </Button>
-              </TableCell>
+
+      {/* Scrollable Table */}
+      <div className="table-container">
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableHeaderCell>Name</TableHeaderCell>
+              <TableHeaderCell>Key</TableHeaderCell>
+              <TableHeaderCell>Unverified rewards</TableHeaderCell>
+              <TableHeaderCell>Verified rewards</TableHeaderCell>
+              <TableHeaderCell>Stake Amount High</TableHeaderCell>
+              <TableHeaderCell>Stake Amount Low</TableHeaderCell>
+              <TableHeaderCell>Added on</TableHeaderCell>
+              <TableHeaderCell>Actions</TableHeaderCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHead>
+          <TableBody>
+            {products.map((product) => (
+              <TableRow key={product.wix_id}>
+                <TableCell>{product.name}</TableCell>
+                <TableCell>
+                  <Text>{product.key}</Text>
+                </TableCell>
+                <TableCell>
+                  <Text>{product.reward.unverified}</Text>
+                </TableCell>
+                <TableCell>
+                  <Text>{product.reward.verified}</Text>
+                </TableCell>
+                <TableCell>
+                  <Text>{product.reward.stake.stake_one}</Text>
+                </TableCell>
+                <TableCell>
+                  <Text>{product.reward.stake.stake_two}</Text>
+                </TableCell>
+                <TableCell>
+                  <Text>
+                    {product.created_at
+                      ? formatDate(product.created_at)
+                      : 'Unknown'}
+                  </Text>
+                </TableCell>
+                <TableCell>
+                  <Button
+                    variant="secondary"
+                    onClick={() => openEditModal(product)}
+                  >
+                    Edit
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+
+      {/* Edit Modal */}
       <Modal
         isOpen={!!editingProduct}
         onRequestClose={closeModal}
@@ -187,27 +246,56 @@ export default function ProductsTable({ products, updateProducts }: { products: 
         style={customStyles}
         contentLabel="Edit Product"
       >
-        <h2 className='mb-4'><strong>Editting</strong> {editingProduct?.name} - ({editingProduct?.key})</h2>
+        <h2 className="mb-4">
+          <strong>Editing</strong> {editingProduct?.name} - (
+          {editingProduct?.key})
+        </h2>
         <form onSubmit={handleSubmit}>
-          <div className='mb-2'>
+          <div className="mb-2">
             <label>Unverified Reward:</label>
-            <NumberInput ref={unverifiedRewardRef} defaultValue={editingProduct?.reward.unverified} step={0.01} />
+            <NumberInput
+              ref={unverifiedRewardRef}
+              defaultValue={editingProduct?.reward.unverified}
+              step={0.01}
+            />
           </div>
           <div>
             <label>Verified Reward:</label>
-            <NumberInput ref={verifiedRewardRef} defaultValue={editingProduct?.reward.verified} step={0.01} />
+            <NumberInput
+              ref={verifiedRewardRef}
+              defaultValue={editingProduct?.reward.verified}
+              step={0.01}
+            />
           </div>
-          <div className='mb-4 mt-4'>
-            <Button type="submit" className='mr-2' variant="primary">Update</Button>
-            <Button onClick={closeModal} variant="secondary">Cancel</Button>
+          <div>
+            <label>Stake Amount High:</label>
+            <NumberInput
+              ref={stakeAmountHighRef}
+              defaultValue={editingProduct?.reward.stake.stake_one}
+              step={0.01}
+            />
+          </div>
+          <div>
+            <label>Stake Amount Low:</label>
+            <NumberInput
+              ref={stakeAmountLowRef}
+              defaultValue={editingProduct?.reward.stake.stake_two}
+              step={0.01}
+            />
+          </div>
+          <div className="mb-4 mt-4">
+            <Button type="submit" className="mr-2" variant="primary">
+              Update
+            </Button>
+            <Button onClick={closeModal} variant="secondary">
+              Cancel
+            </Button>
           </div>
         </form>
       </Modal>
     </div>
-
   );
 }
-
 
 const customStyles = {
   content: {
@@ -218,7 +306,7 @@ const customStyles = {
     marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
     backgroundColor: 'white', // Example background color
-    color: "#6b7280",
+    color: '#6b7280',
     padding: '20px',
     borderRadius: '10px',
     boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)'
--- pages/api/edit-product.ts
+++ pages/api/edit-product.ts
@@ -1,60 +1,76 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import clientPromise from "../../lib/mongoclient";
-import { getServerSession } from "next-auth/next"
-import { authOptions } from "./auth/[...nextauth]";
-import mongoose from "mongoose";
+import { NextApiRequest, NextApiResponse } from 'next';
+import clientPromise from '../../lib/mongoclient';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from './auth/[...nextauth]';
+import mongoose from 'mongoose';
 
 interface ProductData {
-    productId: string;
-    unverifiedReward?: string;
-    verifiedReward?: string;
-    password: string
+  productId: string;
+  unverifiedReward?: string;
+  verifiedReward?: string;
+  stakeAmountHigh?: string;
+  stakeAmountLow: string;
+  password: string;
 }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const session = await getServerSession(req, res, authOptions);
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const session = await getServerSession(req, res, authOptions);
 
-    // Check if user is authenticated and is an admin
-    if (!session || !session.user.admin || !session.user.owner) {
-        res.status(401).json({ message: "Unauthorized" });
-        return;
+  // Check if user is authenticated and is an admin
+  if (!session || !session.user.admin || !session.user.owner) {
+    res.status(401).json({ message: 'Unauthorized' });
+    return;
+  }
+
+  const client = await clientPromise;
+  const db = client.db();
+  const collection = db.collection('products');
+
+  if (req.method === 'PUT') {
+    const data: ProductData = req.body;
+    const {
+      productId,
+      unverifiedReward,
+      verifiedReward,
+      stakeAmountHigh,
+      stakeAmountLow
+    } = data;
+
+    console.log('Updating product', productId);
+
+    const existingProduct = await collection.findOne({
+      wix_id: productId
+    });
+
+    if (!existingProduct) {
+      res.status(404).json({ message: 'Product not found' });
+      return;
     }
 
-    const client = await clientPromise;
-    const db = client.db();
-    const collection = db.collection('products');
+    try {
+      // Construct an update object
+      const updateData: any = {};
+      if (unverifiedReward !== undefined)
+        updateData['reward.unverified'] = +unverifiedReward;
+      if (verifiedReward !== undefined)
+        updateData['reward.verified'] = +verifiedReward;
+      if (stakeAmountHigh !== undefined)
+        updateData['reward.stake.stake_one'] = +stakeAmountHigh;
+      if (stakeAmountLow !== undefined)
+        updateData['reward.stake.stake_two'] = +stakeAmountLow;
 
-    if (req.method === 'PUT') {
-        const data: ProductData = req.body;
-        const { productId, unverifiedReward, verifiedReward } = data;
-        
-        
-        console.log("Updating product", productId);
+      // Update the product in the database
+      await collection.updateOne({ wix_id: productId }, { $set: updateData });
 
-        const existingProduct = await collection.findOne({
-            wix_id: productId
-        });
-
-        if (!existingProduct) {
-            res.status(404).json({ message: "Product not found" });
-            return;
-        }
-
-        try {
-            // Construct an update object
-            const updateData: any = {};
-            if (unverifiedReward !== undefined) updateData['reward.unverified'] = +unverifiedReward;
-            if (verifiedReward !== undefined) updateData['reward.verified'] = +verifiedReward;
-
-            // Update the product in the database
-            await collection.updateOne({ wix_id: productId }, { $set: updateData });
-
-            res.status(200).json({ message: "Product updated successfully" });
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ message: "Error updating product" });
-        }
-    } else {
-        res.status(405).json({ message: "Method Not Allowed" });
+      res.status(200).json({ message: 'Product updated successfully' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error updating product' });
     }
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
 }
