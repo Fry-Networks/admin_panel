d697168c79d45e70542a7c39aae05a74499628b9
Yoyu <157239347+yoyumiracle@users.noreply.github.com>
2024-10-07 09:26:45 -0700

    start ui for reduction page

--- pages/reduction.tsx
+++ pages/reduction.tsx
@@ -1,7 +1,10 @@
+import { Title } from "@tremor/react";
+
 export default function ReductionPage (reduction = []) {
     return (
         <main className="p-4 md:p-10 mx-auto max-w-8xl">
-
+            <Title>Reduction</Title>
+            
         </main>
     )
 }
