8ebd221820919d8a4a1a145e4916457015057a4a
yoyumiracle <157239347+yoyumiracle@users.noreply.github.com>
2025-02-03 18:29:19 +0900

    fix issues suggested by Ator and Dude

--- pages/api/add-license.ts
+++ pages/api/add-license.ts
@@ -1,46 +1,64 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import clientPromise from "../../lib/mongoclient";
-import { getServerSession } from "next-auth/next"
-import { authOptions } from "./auth/[...nextauth]";
-import mongoose from "mongoose";
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const session = await getServerSession(req, res, authOptions);
+import { NextApiRequest, NextApiResponse } from 'next';
+import clientPromise from '../../lib/mongoclient';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from './auth/[...nextauth]';
+import mongoose from 'mongoose';
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const session = await getServerSession(req, res, authOptions);
 
-   
-    if (!session || !session.user.admin) {
-        res.status(401).json({ message: "Unauthorized" });
-        return;
-    }
+  if (!session || !session.user.admin) {
+    res.status(401).json({ message: 'Unauthorized' });
+    return;
+  }
 
-    const client = await clientPromise;
-    const db = client.db('main');
-    const collection = db.collection('byods');
+  const client = await clientPromise;
+  const db = client.db('main');
+  const collection = db.collection('byods');
 
-    if (req.method === 'PUT') {
-        const data: {
-            license: string, email: string
-        } = req.body;
+  if (req.method === 'PUT') {
+    const data: {
+      license: string;
+      email: string;
+    } = req.body;
 
+    console.log(data);
 
-        console.log(data);
-        
-        try {
-            await collection.updateOne({ email: data.email }, {
-                $push: {
-                    licenses: {
-                        license: data.license,
-                        used: false
-                    }
-                }
-            });
+    try {
+      const existOne = await collection.findOne({ email: data.email });
 
-            console.log(`License ${data.license} added successfully by ${session.user.email}`);
-            res.status(200).json({ message: "License added successfully" });
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ message: "Error adding license" });
+      if (!existOne) {
+        await collection.insertOne({
+          email: data.email,
+          algo: false,
+          fry: false,
+          adder: session.user.email
+        });
+      }
+
+      await collection.updateOne(
+        { email: data.email },
+        {
+          $push: {
+            licenses: {
+              license: data.license,
+              used: false
+            }
+          }
         }
-    } else {
-        res.status(405).json({ message: "Method Not Allowed" });
+      );
+
+      console.log(
+        `License ${data.license} added successfully by ${session.user.email}`
+      );
+      res.status(200).json({ message: 'License added successfully' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error adding license' });
     }
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
 }
